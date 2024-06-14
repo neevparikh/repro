66260dcbdebab79c9511a22f6d398ed85d7f4bff
Neev Parikh <neev.v.parikh@gmail.com>
2024-06-14 09:49:58 -0700

    feat: model done, working on training script

--- model/modules.py
+++ model/modules.py
@@ -1,14 +1,25 @@
-from typing import Optional
-import torch
+from dataclasses import dataclass
 from math import sqrt
+
+import torch
 import torch.nn as nn
 
 
+@dataclass
+class Config:
+    context_size: int = 1094
+    vocab_size: int = 50257
+    dim_model: int = 256
+    dim_mlp: int = 512
+    heads: int = 4
+    layers: int = 4
+
+
 class LayerNorm(nn.Module):
-    def __init__(self, features, eps=1e-6):
+    def __init__(self, dim: int, eps: float = 1e-6):
         super(LayerNorm, self).__init__()
-        self.a_2 = nn.Parameter(torch.ones(features))
-        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.a_2 = nn.Parameter(torch.ones(dim))
+        self.b_2 = nn.Parameter(torch.zeros(dim))
         self.eps = eps
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -18,110 +29,149 @@ class LayerNorm(nn.Module):
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, heads, dim_model) -> None:
-        super()
-        assert dim_model % heads == 0
-        self.dim_model = dim_model
-        self.heads = heads
-        self.dim_per_head = dim_model // heads
+    def __init__(self, config) -> None:
+        super(MultiHeadAttention, self).__init__()
+        assert config.dim_model % config.heads == 0
+        self.dim_model = config.dim_model
+        self.heads = config.heads
+        self.dim_per_head = config.dim_model // config.heads
         self.dim_per_head_sqrt = sqrt(self.dim_per_head)
+        self.context_size = config.context_size
 
         self.output_linear = nn.Linear(self.dim_model, self.dim_model)
-        self.query_linear = nn.Linear(self.dim_model, self.dim_model)
-        self.key_linear = nn.Linear(self.dim_model, self.dim_model)
-        self.value_linear = nn.Linear(self.dim_model, self.dim_model)
+        self.combined_linear = nn.Linear(self.dim_model, self.dim_model * 3)
 
-        self.dropout = nn.Dropout(p=0.1)
+        self.register_buffer(
+            "mask",
+            torch.tril(torch.ones(self.context_size, self.context_size)).view(
+                1, 1, self.context_size, self.context_size
+            ),
+        )
 
-    def forward(
-        self,
-        query: torch.Tensor,
-        keys: torch.Tensor,
-        values: torch.Tensor,
-        mask: Optional[torch.Tensor] = None,
-    ) -> torch.Tensor:
-        batch_size = query.size(0)
+    def forward(self, x: torch.Tensor, should_mask: bool) -> torch.Tensor:
+        batch_size, token_sequence_length, embedding_dim = x.size()
+        assert embedding_dim == self.dim_model
 
-        query = (
-            self.query_linear(query)
-            .view(batch_size, -1, self.heads, self.dim_per_head)
-            .transpose(1, 2)
-        )
-        values = (
-            self.values_linear(values)
-            .view(batch_size, -1, self.heads, self.dim_per_head)
-            .transpose(1, 2)
-        )
-        keys = (
-            self.keys_linear(keys)
-            .view(batch_size, -1, self.heads, self.dim_per_head)
-            .transpose(1, 2)
-        )
+        (query, key, value) = self.combined_linear(x).split(
+            self.dim_model, dim=2
+        )  # All are (B x T x M)
+
+        queries = query.view(
+            batch_size, token_sequence_length, self.heads, self.dim_per_head
+        ).transpose(
+            1, 2
+        )  # (B x H x T x M/H)
+        keys = key.view(
+            batch_size, token_sequence_length, self.heads, self.dim_per_head
+        ).transpose(
+            1, 2
+        )  # (B x H x T x M/H)
+        values = value.view(
+            batch_size, token_sequence_length, self.heads, self.dim_per_head
+        ).transpose(
+            1, 2
+        )  # (B x H x T x M/H)
 
         compatibility_scores = (
-            torch.matmul(query, keys.transpose(-2, -1)) / self.dim_per_head_sqrt
-        )
+            torch.matmul(queries, keys.transpose(-2, -1)) / self.dim_per_head_sqrt
+        )  # (B x H x T x M/H) @ (B x H x M/H x T) -> B x H x T x T
 
-        if mask:
-            mask = mask.unsqueeze(1)
-            compatibility_scores = compatibility_scores.masked_fill(mask == 0, -1e9)
+        if should_mask:
+            compatibility_scores = compatibility_scores.masked_fill(
+                self.mask[:, :, :token_sequence_length, :token_sequence_length] == 0,
+                float("-inf"),
+            )
 
-        p_attn = self.dropout(compatibility_scores.softmax(dim=-1))
+        p_attn = compatibility_scores.softmax(dim=-1)
 
-        attended_output = torch.matmul(p_attn, values)
+        attended_output = torch.matmul(
+            p_attn, values
+        )  # (B x H x T x T) @ (B x H x T x M/H) -> (B x H x T x M/H)
+
         attended_output = (
-            attended_output.transpose(1, 2)
+            attended_output.transpose(1, 2)  # (B x T x H x M/H)
             .contiguous()
-            .view(batch_size, -1, self.dim_model)
-        )
+            .view(batch_size, token_sequence_length, self.dim_model)
+        )  # (B x T x M)
 
-        return self.output_linear(attended_output)
+        return self.output_linear(attended_output)  # (B x T x M)
 
 
-class PointwiseFeedForward(nn.Module):
-    def __init__(self, dim_model, dim_feedforward) -> None:
-        super()
-        self.w1 = nn.Linear(dim_model, dim_feedforward)
-        self.w2 = nn.Linear(dim_feedforward, dim_model)
-        self.dropout = nn.Dropout(p=0.1)
+class MLP(nn.Module):
+    def __init__(self, config) -> None:
+        super(MLP, self).__init__()
+        self.w1 = nn.Linear(config.dim_model, config.dim_mlp)
+        self.w2 = nn.Linear(config.dim_mlp, config.dim_model)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.w2(self.dropout(self.w1(x).relu()))
+        x = self.w1(x)  # (B x T x ML)
+        x = nn.functional.gelu(x)
+        return self.w2(x)  # (B x T x M)
 
 
-class EncoderLayer(nn.Module):
-    def __init__(self) -> None:
-        super()
-        dim_model = 128
-        dim_mlp = 512
-
-        self.attention = MultiHeadAttention(heads=2, dim_model=dim_model)
+class DecoderLayer(nn.Module):
+    def __init__(self, config: Config) -> None:
+        super(DecoderLayer, self).__init__()
+        self.attention = MultiHeadAttention(config)
         self.attention_dropout = nn.Dropout(p=0.1)
-        self.attention_layer_norm = LayerNorm(dim_model)
+        self.attention_layer_norm = LayerNorm(config.dim_model)
 
-        self.mlp = nn.Sequential(
-            nn.Linear(dim_model, dim_mlp),
-            nn.ReLU(),
-            nn.Linear(dim_mlp, dim_model),
-        )
+        self.mlp = MLP(config)
         self.mlp_dropout = nn.Dropout(p=0.1)
-        self.mlp_layer_norm = LayerNorm(dim_model)
+        self.mlp_layer_norm = LayerNorm(config.dim_model)
 
-    def forward(self, x, mask=None):
-        if mask is not None:
-            mask = mask.unsqueeze(dim=-1)
-
+    def forward(self, x, should_mask: bool) -> torch.Tensor:
         x = self.attention_layer_norm(x)
-        z, _ = self.attention(x, x, x, mask=mask)
+        z = self.attention(x, should_mask)  # (B x T x M)
         z = x + self.attention_dropout(z)
 
         z = self.mlp_layer_norm(z)
-        r = self.mlp(z)
+        r = self.mlp(z)  # (B x T x M)
         r = z + self.mlp_dropout(r)
 
-        return r
+        return r  # (B x T x M)
 
 
-class Encoder(nn.Module):
-    def __init__(self) -> None:
-        super()
+class Decoder(nn.Module):
+    def __init__(self, config) -> None:
+        super(Decoder, self).__init__()
+        self.layers = nn.ModuleList([DecoderLayer(config) for _ in range(3)])
+        self.last_norm = LayerNorm(config.dim_model)
+
+    def forward(self, x, should_mask) -> torch.Tensor:
+        for layer in self.layers:
+            x = layer(x, should_mask)
+        return self.last_norm(x)  # (B x T x M)
+
+
+class Embeddings(nn.Module):
+    def __init__(self, config) -> None:
+        super(Embeddings, self).__init__()
+        self.token_embeddings = nn.Embedding(config.vocab_size, config.dim_model)
+        self.positional_embeddings = nn.Embedding(config.vocab_size, config.dim_model)
+
+    def forward(self, indices) -> torch.Tensor:
+        _, token_sequence_length = indices.size()  # (B x T)
+        tokens = self.token_embeddings(indices)  # (B x T x M)
+        positions = self.positional_embeddings(
+            torch.arange(
+                0, token_sequence_length, dtype=torch.long, device=indices.device
+            )
+        )  # (T x M)
+        return tokens + positions  # (B x T x M)
+
+
+class Transformer(nn.Module):
+    def __init__(self, config=Config) -> None:
+        super(Transformer, self).__init__()
+        self.embeddings = Embeddings(config)
+        self.decoder = Decoder(config)
+        self.prediction_head = nn.Linear(
+            config.dim_model, config.vocab_size, bias=False
+        )
+
+    def forward(self, indices, should_mask=True) -> torch.Tensor:
+        embeddings = self.embeddings(indices)  # (B x T x M)
+        attended_embeddings = self.decoder(embeddings, should_mask)  # (B x T x M)
+        logits = self.prediction_head(attended_embeddings)  # (B x T x V)
+        return logits.softmax(dim=-1)  # (B x T x V)
--- train.py
+++ train.py
@@ -1,5 +1,38 @@
-from datasets import load_dataset
+from datasets import DatasetDict, load_dataset
+from numerize.numerize import numerize
+import torch
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
 
-data = load_dataset("stas/openwebtext-10k")
+from model.modules import Transformer, Config
 
-print(data)
+dataset = load_dataset("stas/openwebtext-10k")
+assert isinstance(dataset, DatasetDict)
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+assert tokenizer.vocab_size == Config.vocab_size
+
+dataset = dataset.map(
+    lambda data: tokenizer(
+        data["text"],
+        return_tensors="np",
+        padding="max_length",
+        truncation="longest_first",
+        max_length=Config.context_size,
+    ),
+    batched=True,
+)
+dataset = dataset.with_format("torch")
+training_data = dataset["train"]
+
+dataloader = DataLoader(training_data, batch_size=64)  # type: ignore
+
+for batch in dataloader:
+    __import__("pdb").set_trace()
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = Transformer()
+model.eval()
+model.to(device)
+print(f"Model parameters: {numerize(sum([p.numel() for p in model.parameters()]))}")
